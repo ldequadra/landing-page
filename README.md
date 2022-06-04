6978dca680fa962b52b4aac1b19d865ef01a13ac
Luiz Claudio de Quadra <56608210+ldequadra@users.noreply.github.com>
2022-06-04 06:00:36 +0200

    Update final version and README

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Landing Page
 
-Simple landing page with a email subscription form.
+Interface of a simple landing page with an email subscription form.
+
+
+## Color Reference
+
+| Color           | Hex                                                             |
+| --------------- | --------------------------------------------------------------- |
+| Primary         | ![#F2F2F2](http://via.placeholder.com/10/F2F2F2?text=+) #F2F2F2 |
+| Secondary       | ![#0070F3](http://via.placeholder.com/10/0070F3?text=+) #0070F3 |
+| Secondary hover | ![#1983FF](http://via.placeholder.com/10/1983FF?text=+) #1983FF |
+| Form/Button     | ![#FFFFFF](http://via.placeholder.com/10/FFFFFF?text=+) #FFFFFF |
+| Form hover      | ![#F9F9F9](http://via.placeholder.com/10/F9F9F9?text=+) #F9F9F9 |
+| Font            | ![#303030](http://via.placeholder.com/10/303030?text=+) #303030 |
--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import firebase_logo_white from './img/firebase-logo-white.png';
+import firebase_logo_color from './img/firebase-logo-color.png';
 
 function App() {
   return (
+    <div className='container'>
+      <h4 className='company-name'>Company Name</h4>        
+      <h1 className='title'>Welcome to the future of landing pages</h1>
+      <p><a className='a__orange'>Company name helps you!</a> Type your email below and keep informed with all the news about our business. Our tool sends you real-time data.</p>
 
-    <div className="container">
-      <h1>Hello, world!</h1>
+      <div className='form'>
+        <input className='email-input' placeholder='Email address' type='text'></input>
+        <button className='submit-button'>Submit</button>
+      </div>
+
+      <div className='socialbar'>
+        <a href='https://github.com/ldequadra'><i class="fa-brands fa-github"></i></a>
+        <a href='https://twitter.com/ldequadra'><i class="fa-brands fa-twitter"></i></a>
+      </div>
+      <p className='footer'>Developed by <a href='https://github.com/ldequadra'>Luiz de Quadra</a></p>
     </div>
   );
 }
